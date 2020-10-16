303cb19a22973b3611f46bbb74bca0d3888c420b
Sergio Ruy <sergio_ruy@hotmail.com>
2020-10-16 10:43:30 -0300

    SecurityConfig in progress.

--- src/main/java/com/sergioruy/personalblog/service/AuthService.java
+++ src/main/java/com/sergioruy/personalblog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.sergioruy.personalblog.dto.RegisterRequest;
 import com.sergioruy.personalblog.model.User;
 import com.sergioruy.personalblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,19 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUserName());
-        user.setPassword(registerRequest.getPassword());
         user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassoword(registerRequest.getPassword()));
+
         userRepository.save(user);
+    }
+
+    private String encodePassoword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
